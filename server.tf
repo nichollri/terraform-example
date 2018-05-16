d752ab30b9fe6fbd87dfb7054480b8b741c5da78
Shishir Amin <samin@pivotal.io>
2018-05-16 13:42:35 -0400

    changes from presentation/upload slides

--- server.tf
+++ server.tf
@@ -6,26 +6,26 @@ resource "aws_instance" "demo1" {
   subnet_id = "${aws_subnet.demo.id}"
 }
 
-#resource "null_resource" "webpage" {
-#  triggers {
-#    server = "${aws_instance.demo1.id}"
-#  }
-#
-#  connection {
-#    type = "ssh"
-#    host = "${aws_eip.demo.public_ip}"
-#    user = "ubuntu"
-#    private_key = "${tls_private_key.demo.private_key_pem}"
-#  }
-#
-#  provisioner "file" {
-#    source = "files/index.html"
-#    destination = "~/index.html"
-#  }
-#
-#  provisioner "remote-exec" {
-#    inline = [
-#      "sudo cp ~/index.html /var/www/html/index.html"
-#    ]
-#  }
-#}
+resource "null_resource" "webpage" {
+  triggers {
+    server = "${aws_instance.demo1.id}"
+  }
+
+  connection {
+    type = "ssh"
+    host = "${aws_eip.demo.public_ip}"
+    user = "ubuntu"
+    private_key = "${tls_private_key.demo.private_key_pem}"
+  }
+
+  provisioner "file" {
+    source = "files/index.html"
+    destination = "~/index.html"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo cp ~/index.html /var/www/html/index.html"
+    ]
+  }
+}
